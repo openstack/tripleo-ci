f0e70d932820a7ea6ae7b1305f48f9f98b059d9d
Dan Prince <dprince@redhat.com>
2013-05-16 13:13:48 -0400

    Add bm_poseur configuration.

--- toci_test.sh
+++ toci_test.sh
@@ -12,3 +12,14 @@ source $TOCI_WORKING_DIR/stackrc
 
 unset http_proxy
 nova list
+
+sudo $TOCI_WORKING_DIR/tripleo_bm_poseur/bm_poseur --vms 1 --arch i686 create-vm
+MAC=`$TOCI_WORKING_DIR/tripleo_bm_poseur/bm_poseur get-macs`
+
+nova keypair-add --pub-key ~/.ssh/id_rsa.pub default
+nova baremetal-node-create ubuntu 1 512 10 $MAC
+
+for x in {0..30} ; do
+  ssh_noprompt root@$BOOTSTRAP_IP grep 'Free VCPUS: 1' /var/log/upstart/nova-compute.log  && break || true
+  sleep 10
+done
