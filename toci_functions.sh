a87116ad7c6b93470f0ab911aca0120864202acf
Dan Prince <dprince@redhat.com>
2013-06-08 16:59:14 -0400

    Only apply patches if dir exists.

--- toci_functions.sh
+++ toci_functions.sh
@@ -35,9 +35,11 @@ wait_for(){
 
 apply_patches(){
     cd $TOCI_WORKING_DIR/$1
-    for PATCH in $(find $TOCI_SOURCE_DIR/patches/ -name "$2") ; do
-        patch -p1 -N < $PATCH || echo Error : could not apply $PATCH >> $TOCI_LOG_DIR/error-applying-patches.log
-    done
+    if [ -d "$TOCI_SOURCE_DIR/patches/" ]; then
+      for PATCH in $(find $TOCI_SOURCE_DIR/patches/ -name "$2") ; do
+          patch -p1 -N < $PATCH || echo Error : could not apply $PATCH >> $TOCI_LOG_DIR/error-applying-patches.log
+      done
+    fi
 }
 
 mark_time(){
