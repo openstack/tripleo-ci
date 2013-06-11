ea22bc29f82257933fbad37e7ba7a8066cf4e94c
Derek Higgins <derekh@redhat.com>
2013-06-11 08:05:34 +0100

    Remove echo statements
    whoops

--- toci_cleanup.sh
+++ toci_cleanup.sh
@@ -3,6 +3,6 @@
 set -x
 
 for NAME in $(sudo virsh list --name --all | grep "^\(bootstrap\|baremetal_[0-9]\)$"); do
-  echo sudo virsh destroy $NAME
-  echo sudo virsh undefine --remove-all-storage $NAME
+  sudo virsh destroy $NAME
+  sudo virsh undefine --remove-all-storage $NAME
 done
