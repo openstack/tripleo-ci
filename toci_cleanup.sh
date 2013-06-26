ca1224f13f03b309fdc49417999643fea6fb697f
Derek Higgins <derekh@redhat.com>
2013-06-26 19:01:58 +0100

    Remove new seet VM in cleanup

--- toci_cleanup.sh
+++ toci_cleanup.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-for NAME in $(sudo virsh list --name --all | grep "^\(bootstrap\|baremetal_[0-9]\)$"); do
+for NAME in $(sudo virsh list --name --all | grep "^\(seed\|bootstrap\|baremetal_[0-9]\)$"); do
   sudo virsh destroy $NAME
   sudo virsh undefine --remove-all-storage $NAME
 done
