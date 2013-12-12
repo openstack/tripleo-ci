81aa050b82d1d87e33976ae3c8645ca4c93216d0
Derek Higgins <derekh@redhat.com>
2013-12-12 17:15:03 +0000

    Update toci_gate_test.sh to use the broker.
    toci_gate_test now enters the broker and chains into toci_devtest.sh
    via test-envclient. Under the hood this is doing:
    
    $ testenv-client -b <geardserver>:4730 -- ./toci_devtest.sh
    
    This depends on both a gearman broker and at least one registered
    test env being deployed.
    
    toci_devtest includes the code to apply the broker supplied SSH key to
    the system and then attempts to run the first few stages of devtest,
    preserving the test environment definintion from the broker.
    
    The astute of eye may observe that we haven't solved how to pass the gear
    server host into the test environment yet... that may require an
    openstack-infra/config patch.
    
    Change-Id: I53ce4ca687283a924ea055267ea90f20d6632651
    Co-Authored-By: Robert Collins <rbtcollins@hp.com>

---
+++ toci_devtest.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+. toci_functions.sh
+
+if [ ! -e "$TE_DATAFILE" ] ; then
+    echo "Couldn't find data file"
+    exit 1
+fi
+
+PRIV_SSH_KEY=$(OS_CONFIG_FILES=$TE_DATAFILE os-apply-config --key ssh-key --type raw)
+
+echo $PRIV_SSH_KEY | base64 -d > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+
+source ~/tripleo/incubator/scripts/devtest_variables.sh
+devtest_setup.sh --trash-my-machine
+devtest_ramdisk.sh
+devtest_seed.sh
+echo 'Run completed.'
--- toci_gate_test.sh
+++ toci_gate_test.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-echo 'Run completed.'
+
+set -e
+
+# XXX: 127.0.0.1 naturally won't work for real CI but for manual
+# testing running a server on the same machine is convenient.
+GEARDSERVER=${GEARDSERVER:-127.0.0.1}
+
+./testenv-client -b $GEARDSERVER:4730 -- ./toci_devtest.sh
