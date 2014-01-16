e2efc2ce97dcd18d7d038570970ab83014734b29
Derek Higgins <derekh@redhat.com>
2014-01-16 16:39:10 +0000

    Change to tripleo-ci directory
    Various paths in toci are relative from here.
    
    Change-Id: I67c1bdeb5fb33e8426659689e1a807f927a46db0

--- toci_gate_test.sh
+++ toci_gate_test.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# cd to toci directory so relative paths work (below and in toci_devtest.sh)
+cd $(dirname $0)
+
 # XXX: 127.0.0.1 naturally won't work for real CI but for manual
 # testing running a server on the same machine is convenient.
 GEARDSERVER=${GEARDSERVER:-127.0.0.1}
