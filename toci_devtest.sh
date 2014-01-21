129d90f5141e74336eb2013db0c918465464003f
Derek Higgins <derekh@redhat.com>
2014-01-21 16:51:57 +0000

    set DIB_REPOLOCATION_<project> for projects
    Once set disk-image-create will pull dependencies from local disk.
    
    Change-Id: I592ba705a5640d78adf748821f7929617bcb846d

--- toci_devtest.sh
+++ toci_devtest.sh
@@ -18,6 +18,15 @@ chmod 600 ~/.ssh/id_rsa
 ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
+# set DIB_REPOLOCATION_<project> for each of the projects cloned by devstack-vm-gate-wrap.sh
+# built images will then pull git repository dependencies from local disk.
+for GITDIR in $(ls -d /opt/stack/new/*/.git) ; do
+    PROJDIR=${GITDIR%/.git}
+    PROJNAME=${PROJDIR##*/}
+    PROJNAME=${PROJNAME//[-.]/_}
+    export DIB_REPOLOCATION_$PROJNAME=$PROJDIR
+done
+
 export TRIPLEO_ROOT=~/tripleo
 source ~/tripleo/tripleo-incubator/scripts/devtest_variables.sh
 devtest_setup.sh --trash-my-machine
