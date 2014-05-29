c8fe03744441403cbc52d4f1bf09eb39e8a475e9
Dan Prince <dprince@redhat.com>
2014-05-29 14:37:51 -0400

    Set http_proxy to use local rack squid caches
    We are currently running a Squid proxy on 192.168.1.100 in both Racks
    This change may eventually belong in openstack-infra/config but
    we can test and use it here for now...
    
    Change-Id: Ic0aef792a44e473d2b75ad0091420ec1e1c2f4b9

--- toci_gate_test.sh
+++ toci_gate_test.sh
@@ -5,6 +5,11 @@ set -eu
 # cd to toci directory so relative paths work (below and in toci_devtest.sh)
 cd $(dirname $0)
 
+# We are currently running a Squid proxy on 192.168.1.100 in both Racks
+# This change may eventually belong in openstack-infra/config but we
+# can test and use it here for now
+export http_proxy=http://192.168.1.100:3128/
+
 # Download a custom Fedora image here. We want to use an explit URL
 # so that Squid caches this. I'm doing it here to test things for now...
 # Once it works this code actually belongs in prepare_node_tripleo.sh
