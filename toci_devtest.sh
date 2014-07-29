a00c69f5bd581549fa5ffbea807d116cf261a613
Derek Higgins <derekh@redhat.com>
2014-07-29 17:00:32 +0100

    Revert SSH virsh to use the new ManagementInterface
    Our testenvs don't currently allow the new command.
    
    Change-Id: Ia79d9417bb81486131e414853dbe7011e2c8a9e3

--- toci_devtest.sh
+++ toci_devtest.sh
@@ -32,6 +32,8 @@ function temprevert(){
 temprevert nova 963ad71af4750e28745b6de262da11816b403801 1342919
 # https://review.openstack.org/#/c/107042/ (Update Twitter Bootstrap to version 3)
 temprevert horizon 92146772b677e9fce57cc11b4a4a1542a05c23b2 1349774
+# https://review.openstack.org/#/c/89884/ SSH virsh to use the new ManagementInterface
+temprevert ironic a6c6dbd94f98e657f20875699cd6403ccbc33475 1349913
 
 PRIV_SSH_KEY=$(OS_CONFIG_FILES=$TE_DATAFILE os-apply-config --key ssh-key --type raw)
 SEED_IP=$(OS_CONFIG_FILES=$TE_DATAFILE os-apply-config --key seed-ip --type netaddress --key-default '')
