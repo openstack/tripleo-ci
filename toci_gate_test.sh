ac06b8fa39d054a32598a92612d2ce2215a6a5a6
Derek Higgins <derekh@redhat.com>
2015-02-17 09:37:21 +0000

    Pin puppet job to known working delorean repo
    Change-Id: I0cd7d3a3de1091e5a14d1e3cc1cb9489b367b8cc

--- toci_gate_test.sh
+++ toci_gate_test.sh
@@ -50,7 +50,7 @@ for JOB_TYPE_PART in $(sed 's/-/ /g' <<< "${TOCI_JOBTYPE:-}") ; do
             fi
             export ELEMENTS_PATH=$TRIPLEO_ROOT/tripleo-puppet-elements/elements:$TRIPLEO_ROOT/heat-templates/hot/software-config/elements:$TRIPLEO_ROOT/tripleo-image-elements/elements
             export USE_MERGEPY=0
-            export DELOREAN_REPO_URL="http://104.130.230.24/~fedora/repos/current"
+            export DELOREAN_REPO_URL="http://104.130.230.24/f21/0d/97/0d979b220da3c5c809a20d6d9e7afa6b2c246712_772aab0d"
             export RDO_RELEASE=juno
             export DIB_COMMON_ELEMENTS='stackuser os-net-config delorean-repo rdo-release'
             export USE_MARIADB=0
