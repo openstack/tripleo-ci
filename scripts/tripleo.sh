0c092c18736d2525677cc9ac4038b6924ceaeb98
Steven Hardy <shardy@redhat.com>
2016-04-05 11:15:26 +0100

    Test with latest master heat
    Until we move the current-tripleo pin to include this revert, I'm
    not sure our upgrades job will work:
    
    https://review.openstack.org/#/c/299112/
    
    This was reverted due to issues deleting overclouds, but related
    issues are now being seen in CI on update.
    
    Change-Id: If285b7a14cfcf978912cc5a9bedbbe02929cec22
    Related-Bug: #1561172

--- scripts/tripleo.sh
+++ scripts/tripleo.sh
@@ -206,7 +206,7 @@ function repo_setup {
         sudo sed -i 's/\[delorean\]/\[delorean-current\]/' $REPO_PREFIX/delorean-current.repo
         sudo /bin/bash -c "cat <<-EOF>>$REPO_PREFIX/delorean-current.repo
 
-includepkgs=diskimage-builder,instack,instack-undercloud,os-apply-config,os-cloud-config,os-collect-config,os-net-config,os-refresh-config,python-tripleoclient,tripleo-common,openstack-tripleo-heat-templates,openstack-tripleo-image-elements,openstack-tripleo,openstack-tripleo-puppet-elements
+includepkgs=diskimage-builder,instack,instack-undercloud,os-apply-config,os-cloud-config,os-collect-config,os-net-config,os-refresh-config,python-tripleoclient,tripleo-common,openstack-tripleo-heat-templates,openstack-tripleo-image-elements,openstack-tripleo,openstack-tripleo-puppet-elements openstack-heat*
 EOF"
     else
         # Enable the Delorean Deps repository
