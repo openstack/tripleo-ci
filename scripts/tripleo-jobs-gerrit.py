a701f28222d1f35967ad6eda39d36203acbba6a4
Derek Higgins <derekh@redhat.com>
2016-07-05 13:19:04 +0100

    Update ci report script to only include rh2 jobs
    There are other jobs run on rh2, but these are the only two
    that will be regularly run.
    
    Change-Id: Ie01cccefcb57fdf91ed3c74055225f044c0ff275

--- scripts/tripleo-jobs-gerrit.py
+++ scripts/tripleo-jobs-gerrit.py
@@ -8,10 +8,7 @@ import re
 import datetime
 
 DEFAULT_JOB_NAMES = [
-    'gate-tripleo-ci-centos-7-nonha',
-    'gate-tripleo-ci-centos-7-ha',
-    'gate-tripleo-ci-centos-7-upgrades',
-    'gate-tripleo-ci-centos-7-containers',
+    'gate-tripleo-ci-centos-7-ovb-nonha',
     'gate-tripleo-ci-centos-7-ovb-ha',
 ]
 
