72285f5503b22e19edaebffb6d4fcdc31cb81edf
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2016-09-15 11:55:24 +0300

    Also set Haproxy addresses in no_proxy
    When we deploy with SSL we do so via haproxy. So just as we do for
    the undercloud services IP, we need to add that one to the no_proxy
    list.
    
    Change-Id: If158033663878e621c8683c7754b87c227550c75

--- toci_instack_ovb.sh
+++ toci_instack_ovb.sh
@@ -41,7 +41,11 @@ cat ~/.ssh/id_rsa.pub | sudo tee -a ~root/.ssh/authorized_keys | sudo tee -a ~/.
 sudo yum remove -y puppet hiera puppetlabs-release rdo-release
 sudo rm -rf /etc/puppet /etc/hiera.yaml
 
-export no_proxy=192.0.2.1,$MY_IP,$MIRRORSERVER
+undercloud_net_range="192.0.2."
+undercloud_services_ip=$undercloud_net_range"1"
+undercloud_haproxy_public_ip=$undercloud_net_range"2"
+undercloud_haproxy_admin_ip=$undercloud_net_range"3"
+export no_proxy=$undercloud_services_ip,$undercloud_haproxy_public_ip,$undercloud_haproxy_admin_ip,$MY_IP,$MIRRORSERVER
 
 # Setup delorean
 $TRIPLEO_ROOT/tripleo-ci/scripts/tripleo.sh --delorean-setup
