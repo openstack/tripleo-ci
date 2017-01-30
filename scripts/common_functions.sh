f11aa57461cb3db1f7011105aa5dad382a33440a
Sagi Shnaidman <sshnaidm@redhat.com>
2017-01-30 13:15:08 +0200

    Exclude grenade from building with DLRN
    Exclude grenade project from being built with DLRN in CI
    
    Change-Id: I4d828430be505aaa5c4e6a3ab7f856c6bc06a9f5

--- scripts/common_functions.sh
+++ scripts/common_functions.sh
@@ -263,7 +263,7 @@ function delorean_build_and_serve {
     for PROJFULLREF in $ZUUL_CHANGES ; do
         PROJ=$(filterref $PROJFULLREF)
         # If ci is being run for a change to ci its ok not to have a ci produced repository
-        excluded_proj="tripleo-ci tripleo-quickstart tripleo-quickstart-extras puppet-openstack-integration"
+        excluded_proj="tripleo-ci tripleo-quickstart tripleo-quickstart-extras puppet-openstack-integration grenade"
         if [[ " $excluded_proj " =~ " $PROJ " ]]; then
             mkdir -p $TRIPLEO_ROOT/delorean/data/repos/current
             touch $TRIPLEO_ROOT/delorean/data/repos/current/delorean-ci.repo
