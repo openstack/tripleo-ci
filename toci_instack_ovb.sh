f61c5e51d8ad028f212d642ecdd6b49408db9a80
Sagi Shnaidman <sshnaidm@redhat.com>
2017-04-02 17:23:33 +0300

    Set REMAINING_TIME for local run
    When running CI scripts locally it fails because REMAINING_TIME is
    propagated by infra before the job starts. This patch sets it to
    some default value if it wasn't set before.
    
    Change-Id: I0050046db17e50ddd17a7115e46c4684afa87b2f

--- toci_instack_ovb.sh
+++ toci_instack_ovb.sh
@@ -112,6 +112,7 @@ cp -f $TE_DATAFILE ~/instackenv.json
 
 # Use $REMAINING_TIME of infra to calculate maximum time for remaning part of job
 # Leave 10 minutes for postci function
+REMAINING_TIME=${REMAINING_TIME:-180}
 TIME_FOR_DEPLOY=$(( REMAINING_TIME - ($(date +%s) - START_JOB_TIME)/60 - 10 ))
 /usr/bin/timeout --preserve-status ${TIME_FOR_DEPLOY}m $TRIPLEO_ROOT/tripleo-ci/scripts/deploy.sh
 
