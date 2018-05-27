3dab6f0df04aa45c18a43526e15baad428130df7
Ronelle Landy <rlandy@redhat.com>
2018-05-27 17:53:11 -0400

    Add dry run option to toci_quickstart - playbook order in tact
    With dry run, playbooks are not executed.
    The env variables, releases script output and playbooks
    commands are written out to files for debug.
    
    The review also moves the collect logs lines to a
    function for easier organization
    
    Documentation for the dry run option and variable
    files is added.
    
    Change-Id: I0bad5ee3150d94115bed018de9488590305a2b80

--- toci_quickstart.sh
+++ toci_quickstart.sh
@@ -121,94 +121,55 @@ set -u
 source $OOOQ_DIR/ansible_ssh_env.sh
 [[ -n ${STATS_OOOQ:-''} ]] && export STATS_OOOQ=$(( $(date +%s) - STATS_OOOQ ))
 
-
-
-for playbook in $PLAYBOOKS; do
-    echo "${RELEASE_ARGS[$playbook]:=$QUICKSTART_DEFAULT_RELEASE_ARG}"
-    run_with_timeout $START_JOB_TIME $QUICKSTART_INSTALL_CMD \
-       "${RELEASE_ARGS[$playbook]:=$QUICKSTART_DEFAULT_RELEASE_ARG}" \
-       $NODES_ARGS \
-       $FEATURESET_CONF \
-       $ENV_VARS \
-       $EXTRA_VARS \
-       $DEFAULT_ARGS \
-       --extra-vars ci_job_end_time=$(( START_JOB_TIME + REMAINING_TIME*60 )) \
-        $LOCAL_WORKING_DIR/playbooks/$playbook "${PLAYBOOKS_ARGS[$playbook]:-}" \
-        2>&1 | tee -a $LOGS_DIR/quickstart_install.log && exit_value=0 || exit_value=$?
-
-    # Print status of playbook run
-    [[ "$exit_value" == 0 ]] && echo "Playbook run of $playbook passed successfully"
-    [[ "$exit_value" != 0 ]] && echo "Playbook run of $playbook failed" && break
-done
-
-[[ "$exit_value" == 0 ]] && echo "Playbook run passed successfully" || echo "Playbook run failed"
-
-## LOGS COLLECTION
-
-cat <<EOF > $LOGS_DIR/collect_logs.sh
-#!/bin/bash
-set -x
-
-export NODEPOOL_PROVIDER=${NODEPOOL_PROVIDER:-''}
-export STATS_TESTENV=${STATS_TESTENV:-''}
-export STATS_OOOQ=${STATS_OOOQ:-''}
-export START_JOB_TIME=${START_JOB_TIME:-''}
-export ZUUL_PIPELINE=${ZUUL_PIPELINE:-''}
-export DEVSTACK_GATE_TIMEOUT=${DEVSTACK_GATE_TIMEOUT:-''}
-export REMAINING_TIME=${REMAINING_TIME:-''}
-export LOCAL_WORKING_DIR="$WORKSPACE/.quickstart"
-export OPT_WORKDIR=$LOCAL_WORKING_DIR
-export WORKING_DIR="$HOME"
-export LOGS_DIR=$WORKSPACE/logs
-export VIRTUAL_ENV_DISABLE_PROMPT=1
-export ANSIBLE_CONFIG=$OOOQ_DIR/ansible.cfg
-export ARA_DATABASE=sqlite:///${LOCAL_WORKING_DIR}/ara.sqlite
-export ZUUL_CHANGES=${ZUUL_CHANGES:-''}
-export NODES_FILE=${NODES_FILE:-''}
-export TOCI_JOBTYPE=$TOCI_JOBTYPE
-export STABLE_RELEASE=${STABLE_RELEASE:-''}
-export QUICKSTART_RELEASE=${QUICKSTART_RELEASE:-''}
-
-set +u
-source $LOCAL_WORKING_DIR/bin/activate
-set -u
-source $OOOQ_DIR/ansible_ssh_env.sh
-
-/usr/bin/timeout --preserve-status 40m $QUICKSTART_COLLECTLOGS_CMD  > $LOGS_DIR/quickstart_collect_logs.log || \
-    echo "WARNING: quickstart collect-logs failed, check quickstart_collectlogs.log for details"
-
-cp $LOGS_DIR/undercloud/var/log/postci.txt.gz $LOGS_DIR/ || true
-
-if [[ -e $LOGS_DIR/undercloud/home/$USER/tempest/testrepository.subunit.gz ]]; then
-    cp $LOGS_DIR/undercloud/home/$USER/tempest/testrepository.subunit.gz ${LOGS_DIR}/testrepository.subunit.gz
-elif [[ -e $LOGS_DIR/undercloud/home/$USER/pingtest.subunit.gz ]]; then
-    cp $LOGS_DIR/undercloud/home/$USER/pingtest.subunit.gz ${LOGS_DIR}/testrepository.subunit.gz
-elif [[ -e $LOGS_DIR/undercloud/home/$USER/undercloud_sanity.subunit.gz ]]; then
-    cp $LOGS_DIR/undercloud/home/$USER/undercloud_sanity.subunit.gz ${LOGS_DIR}/testrepository.subunit.gz
+# Debug step capture env variables
+if [[ "$PLAYBOOK_DRY_RUN" == "1" ]]; then
+    echo "-- Capture Environment Variables Used ---------"
+    echo "$(env)" | tee -a $LOGS_DIR/toci_env_args_output.log
+    declare -p | tee -a $LOGS_DIR/toci_env_args_output.log
 fi
 
-# Copy tempest.html to root dir
-cp $LOGS_DIR/undercloud/home/$USER/tempest/tempest.html.gz ${LOGS_DIR} || true
+echo "-- Playbooks Output --------------------------"
+for playbook in $PLAYBOOKS; do
+    echo "$QUICKSTART_INSTALL_CMD \
+        ${RELEASE_ARGS[$playbook]:=$QUICKSTART_DEFAULT_RELEASE_ARG} \
+        $NODES_ARGS \
+        $FEATURESET_CONF \
+        $ENV_VARS \
+        $EXTRA_VARS \
+        $DEFAULT_ARGS \
+        $LOCAL_WORKING_DIR/playbooks/$playbook ${PLAYBOOKS_ARGS[$playbook]:-}" \
+        | sed  's/--/\n--/g' \
+        | tee -a $LOGS_DIR/playbook_executions.log
+    echo "# --------------------------------------- " \
+        | tee -a $LOGS_DIR/playbook_executions.log
+done
 
-# Copy tempest and .testrepository directory to /opt/stack/new/tempest and
-# unzip
-sudo mkdir -p /opt/stack/new
-sudo cp -Rf $LOGS_DIR/undercloud/home/$USER/tempest /opt/stack/new || true
-sudo gzip -d -r /opt/stack/new/tempest/.testrepository || true
+if [[ "$PLAYBOOK_DRY_RUN" == "1" ]]; then
+    exit_value=0
+else
+    for playbook in $PLAYBOOKS; do
+        echo "${RELEASE_ARGS[$playbook]:=$QUICKSTART_DEFAULT_RELEASE_ARG}"
+        run_with_timeout $START_JOB_TIME $QUICKSTART_INSTALL_CMD \
+           "${RELEASE_ARGS[$playbook]:=$QUICKSTART_DEFAULT_RELEASE_ARG}" \
+           $NODES_ARGS \
+           $FEATURESET_CONF \
+           $ENV_VARS \
+           $EXTRA_VARS \
+           $DEFAULT_ARGS \
+           --extra-vars ci_job_end_time=$(( START_JOB_TIME + REMAINING_TIME*60 )) \
+            $LOCAL_WORKING_DIR/playbooks/$playbook "${PLAYBOOKS_ARGS[$playbook]:-}" \
+            2>&1 | tee -a $LOGS_DIR/quickstart_install.log && exit_value=0 || exit_value=$?
 
-# record the size of the logs directory
-# -L, --dereference     dereference all symbolic links
-# Note: tail -n +1 is to prevent the error 'Broken Pipe' e.g. 'sort: write failed: standard output: Broken pipe'
+        # Print status of playbook run
+        [[ "$exit_value" == 0 ]] && echo "Playbook run of $playbook passed successfully"
+        [[ "$exit_value" != 0 ]] && echo "Playbook run of $playbook failed" && break
+    done
 
-du -L -ch $LOGS_DIR/* | tail -n +1 | sort -rh | head -n 200 &> $LOGS_DIR/log-size.txt || true
-EOF
+    [[ "$exit_value" == 0 ]] && echo "Playbook run passed successfully" || echo "Playbook run failed"
 
-if [[ "${NODEPOOL_PROVIDER:-''}" == "rdo-cloud-tripleo" ]] || [[ "${NODEPOOL_PROVIDER:-''}" == "tripleo-test-cloud-rh1" ]]; then
-    if [[ "$TOCI_JOBTYPE" =~ "ovb" ]]; then
-        bash $LOGS_DIR/collect_logs.sh
-        # rename script to not to run it in multinode jobs
-        mv $LOGS_DIR/collect_logs.sh $LOGS_DIR/ovb_collect_logs.sh
-    fi
+    ## LOGS COLLECTION
+    collect_logs
+
 fi
 
 popd
